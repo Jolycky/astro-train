881c1916aade0793ee2898e156f0d266626403bd
Jose Bagus <josebagus4@gmail.com>
2024-07-03 18:59:49 +0700

    initial commit

---
+++ src/data/experience.ts
@@ -0,0 +1,22 @@
+import type { Props as ExperienceProp } from "../components/experience/props";
+
+export const EXPERIENCE: ExperienceProp[] = [
+  {
+    dates: "August 2023 — Present",
+    title: "Cloud & Backend Engineer",
+    company: "Eventhings",
+    companyUrl: "https://www.eventhings.id",
+    description:
+      "Designed and architected the resource architecture, ensuring robust maintenance using Google Cloud Platform. Implemented continuous integration and continuous deployment (CI/CD) pipelines to streamline development and deployment processes. Developed and managed APIs using TypeScript and Express.js, collaborating closely with team members to deliver high-quality solutions.",
+    technologies: [
+      "Google Cloud Platform",
+      "TypeScript",
+      "Express.js",
+      "Node.JS",
+      "Docker",
+      "Github Action",
+      "PostgreSQL",
+    ],
+    logo: "/logos/eventhings.jpeg",
+  },
+];
---
+++ src/data/socials.ts
@@ -0,0 +1,21 @@
+import IconGithub from "../components/icons/github.astro";
+import IconLinkedin from "../components/icons/linkedin.astro";
+import IconTwitter from "../components/icons/twitter.astro"
+
+export const SOCIALS = [
+  {
+    name: "Github",
+    url: "https://github.com/Jolycky",
+    icon: IconGithub,
+  },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/josebagus/",
+    icon: IconLinkedin,
+  },
+  {
+    name: "Twitter",
+    url: "https://twitter.com/jolycky",
+    icon: IconTwitter,
+  }
+];
